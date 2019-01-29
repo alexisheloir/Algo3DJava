0d3173dfeb3aa7b01225aa8392d8a62ce2891a62
Marc Rouillet <rouilletmarc@netcourrier.com>
2019-01-29 17:54:55 +0100

    On a fait des modifications de test : on a remplacé les primitives TRIANGLES par LINES. Voilà. Rien de spécial en plus.

--- TP0/QuadExample.java
+++ TP0/QuadExample.java
@@ -41,7 +41,7 @@ public class QuadExample {
             System.exit(0);
         }
   
-        // init OpenGL
+        // init OpenGL (boucle infinie)
         GL11.glMatrixMode(GL11.GL_PROJECTION);
         GL11.glLoadIdentity();
         GL11.glOrtho(0, 800, 0, 600, 1, -1);
@@ -50,17 +50,74 @@ public class QuadExample {
         while (!Display.isCloseRequested()) {
             // Clear the screen and depth buffer
             GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);  
-         
+            /*
             // set the color of the quad (R,G,B,A)
-            GL11.glColor3f(0.5f,0.5f,1.0f);
+            GL11.glColor3f(1.0f,1.0f,0.0f);
+            
+           
              
             // draw quad
+           
             GL11.glBegin(GL11.GL_QUADS);
             GL11.glVertex2f(100,100);
             GL11.glVertex2f(100+200,100);
             GL11.glVertex2f(100+200,100+200);
             GL11.glVertex2f(100,100+200);
             GL11.glEnd();
+            */
+           
+            //Jaune
+            GL11.glColor3f(1.0f,1.0f,0.0f);
+            
+            GL11.glBegin(GL11.GL_LINES);
+            GL11.glVertex2f(300-100,300-150);
+            GL11.glVertex2f(300,300);
+            GL11.glVertex2f(300+100,300-150);
+            GL11.glEnd();
+            
+            GL11.glColor3f(1.0f,1.0f,0.0f);
+            
+            GL11.glBegin(GL11.GL_LINES);
+            GL11.glVertex2f(300,300);
+            GL11.glVertex2f(300+100,300+150);
+            GL11.glVertex2f(300+200,300);
+            GL11.glEnd();
+            
+            GL11.glColor3f(1.0f,1.0f,0.0f);
+            
+            GL11.glBegin(GL11.GL_LINES);
+            GL11.glVertex2f(300+100,300-150);
+            GL11.glVertex2f(300+200,300);
+            GL11.glVertex2f(300+300,300-150);
+            GL11.glEnd();
+            
+            //Noir
+            GL11.glColor3f(0.0f,0.0f,0.0f);
+            
+            GL11.glBegin(GL11.GL_LINES);
+            GL11.glVertex2f(300-50,300-75);
+            GL11.glVertex2f(300,300-150);
+            GL11.glVertex2f(300+50,300-75);
+            GL11.glEnd();
+            
+            GL11.glColor3f(0.0f,0.0f,0.0f);
+            
+            GL11.glBegin(GL11.GL_LINES);
+            GL11.glVertex2f(350,300+75);
+            GL11.glVertex2f(300+100,300);
+            GL11.glVertex2f(300+150,300+75);
+            GL11.glEnd();
+            
+            GL11.glColor3f(0.0f,0.0f,0.0f);
+            
+            GL11.glBegin(GL11.GL_LINES);
+            GL11.glVertex2f(300+150,300-75);
+            GL11.glVertex2f(300+200,300-150);
+            GL11.glVertex2f(300+250,300-75);
+            GL11.glEnd();
+            
+            
+            
   
             Display.update();
         }
